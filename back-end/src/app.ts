3562f56c03b72c7ab6e8e97dae3bb73fa5f70418
Jochen Szostek <jochen.szostek@gmail.com>
2018-05-05 16:59:44 +0200

    added jwt checking back-end

--- back-end/src/app.ts
+++ back-end/src/app.ts
@@ -1,7 +1,13 @@
-import * as express from 'express';
-import * as bodyParser from 'body-parser';
-import * as logger from 'morgan';
-import * as debug from 'debug';
+import * as express from "express";
+import * as bodyParser from "body-parser";
+import * as logger from "morgan";
+import * as debug from "debug";
+import * as jwt from "express-jwt";
+
+// declare var jwtAuthz: any;
+import * as jwtAuthz from "express-jwt-authz";
+
+import * as jwksRsa from "jwks-rsa";
 
 let log = debug("back-end:app");
 
@@ -12,7 +18,7 @@ class App {
 
   //Run configuration methods on the Express instance.
   constructor() {
-    debug('ts-express:server');
+    debug("ts-express:server");
     this.express = express();
     this.middleware();
     this.routes();
@@ -20,23 +26,54 @@ class App {
 
   // Configure Express middleware.
   private middleware(): void {
-    this.express.use(logger('dev'));
+    this.express.use(logger("dev"));
     this.express.use(bodyParser.json());
     this.express.use(bodyParser.urlencoded({ extended: false }));
   }
 
   // Configure API endpoints.
   private routes(): void {
+
+    const checkScopes = jwtAuthz(["read:messages"]);
+
+    // Authentication middleware. When used, the
+    // Access Token must exist and be verified against
+    // the Auth0 JSON Web Key Set
+    const checkJwt = jwt({
+      // Dynamically provide a signing key
+      // based on the kid in the header and
+      // the signing keys provided by the JWKS endpoint.
+      secret: jwksRsa.expressJwtSecret({
+        cache: true,
+        rateLimit: true,
+        jwksRequestsPerMinute: 5,
+        jwksUri: `https://prefabsoft.eu.auth0.com/.well-known/jwks.json`
+      }),
+
+      // Validate the audience and the issuer.
+      audience: "http://localhost:3000",
+      issuer: `https://prefabsoft.eu.auth0.com/`,
+      algorithms: ["RS256"]
+    });
+
     /* This is just to get up and running, and to make sure what we've got is
          * working so far. This function will change when we start to add more
          * API endpoints */
     let router = express.Router();
     // placeholder route handler
     router.get("/waves", (req, res, next) => {
-      log("Getting \"/waves\" route.");
+      log('Getting "/waves" route.');
       // res.set('Content-Type', 'application/json');
       res.json({
         message: "Hello Waves!"
+      });
+    });
+
+    router.get("/secure", checkJwt/* , checkScopes */, (req, res, next) => {
+      log('Getting "/secure" route.');
+      // res.set('Content-Type', 'application/json');
+      res.json({
+        message: "Hello Secured!"
       });
     });
 
